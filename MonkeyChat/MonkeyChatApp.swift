5dacc1aa32cdf8c2d80fb967854bacdff1d70a8a
Murat Can Koç <muratcankoc@gmail.com>
2022-01-18 20:01:37 +0300

    Initial Commit

---
+++ MonkeyChat/MonkeyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkeyChatApp.swift
+//  MonkeyChat
+//
+//  Created by Murat Can KOÇ on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MonkeyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
